8dc5c698661e32a9b8e72ae0653dd42969d67a38
trevorfar <levelonethousandr@gmail.com>
2024-05-03 01:19:44 -0400

    good

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -18,10 +18,6 @@ export interface Config {
   };
   globals: {};
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "users".
- */
 export interface User {
   id: string;
   role: 'admin' | 'user';
@@ -38,10 +34,6 @@ export interface User {
   lockUntil?: string | null;
   password: string | null;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "products".
- */
 export interface Product {
   id: string;
   user?: (string | null) | User;
@@ -60,10 +52,6 @@ export interface Product {
   updatedAt: string;
   createdAt: string;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "product_files".
- */
 export interface ProductFile {
   id: string;
   user?: (string | null) | User;
@@ -76,10 +64,6 @@ export interface ProductFile {
   width?: number | null;
   height?: number | null;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "media".
- */
 export interface Media {
   id: string;
   user?: (string | null) | User;
@@ -118,10 +102,6 @@ export interface Media {
     };
   };
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "orders".
- */
 export interface Order {
   id: string;
   _isPaid: boolean;
@@ -130,10 +110,6 @@ export interface Order {
   updatedAt: string;
   createdAt: string;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "payload-preferences".
- */
 export interface PayloadPreference {
   id: string;
   user: {
@@ -153,10 +129,6 @@ export interface PayloadPreference {
   updatedAt: string;
   createdAt: string;
 }
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "payload-migrations".
- */
 export interface PayloadMigration {
   id: string;
   name?: string | null;
