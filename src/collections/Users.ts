3efb1b4812eab354e08d796c6902d312f8a824ef
Trevor Farias <levelonethousandr@gmail.com>
2024-05-18 20:45:10 -0400

    good

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,34 +1,77 @@
-import { CollectionConfig } from "payload/types";
+import { PrimaryActionEmailHtml } from "../components/emails/PrimaryActionEmail"
+import { Access, CollectionConfig } from "payload/types"
 
-{/** TODO: POSSIBLE CHANGE, DIFFERENT ROLES MAY NOT BE NECESSARY */ }
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === "admin") return true
+  return {
+    id: {
+      equals: user.id,
+    },
+  }
+}
+{
+  /** TODO: POSSIBLE CHANGE, DIFFERENT ROLES MAY NOT BE NECESSARY */
+}
 export const Users: CollectionConfig = {
-    slug: "users",
-    auth: {
-        verify: {
-            generateEmailHTML: ({token}) => {
-                return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Verify Account</a>`
-            },
-        },
+  slug: "users",
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return PrimaryActionEmailHtml({
+            actionLabel: "verify your account",
+            buttonText: "Veryify Account",
+            href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+        })
+      },
     },
+  },
 
-    access: {
-        read: () => true,
-        create: () => true,
+  access: {
+    read: adminsAndUser,
+    create: () => true,
+    update: ({req}) => req.user.role === "admin",
+    delete: ({req}) => req.user.role === "admin"
+  },
+  admin: {
+    hidden: ({user}) => user.role !== "admin",
+    defaultColumns: ['id'],
+  },
+  fields: [
+    {
+      name: "products",
+      label: "Products",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
     },
-    fields: [
+    {
+      name: "product_files",
+      label: "Product Files",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
+    {
+      name: "role",
+      defaultValue: "user",
+      required: true,
+      type: "select",
+      options: [
         {
-            name: "role",
-            defaultValue: 'user',
-            required: true,
-            type: "select",
-            options: [
-                {
-                    label: "Admin", value: "admin"
-                },
-                {
-                    label: "User", value: "user"
-                },
-            ]
-        }
-    ]
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "User",
+          value: "user",
+        },
+      ],
+    },
+  ],
 }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import express from "express";
-import { getPayloadClient } from "./get-payload";
-import { nextApp, nextHandler } from "./next-utils";
-import * as trpcExpress from "@trpc/server/adapters/express";
-import { appRouter } from "./trpc";
-import { inferAsyncReturnType } from "@trpc/server";
-import bodyParser from "body-parser";
-import { IncomingMessage } from "http";
-import { stripeWebhookHandler } from "./webhooks";
+import express from "express"
+import { getPayloadClient } from "./get-payload"
+import { nextApp, nextHandler } from "./next-utils"
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { appRouter } from "./trpc"
+import { inferAsyncReturnType } from "@trpc/server"
+import bodyParser from "body-parser"
+import { IncomingMessage } from "http"
+import { stripeWebhookHandler } from "./webhooks"
 import nextBuild from "next/dist/build"
-import path from "path";
+import path from "path"
+import { PayloadRequest } from "payload/types"
+import { parse } from "url"
 
-const app = express();
-const PORT = Number(process.env.PORT) || 3000;
+const app = express()
+const PORT = Number(process.env.PORT) || 3000
 
 const createContext = ({
   req,
@@ -19,42 +21,49 @@ const createContext = ({
 }: trpcExpress.CreateExpressContextOptions) => ({
   req,
   res,
-});
+})
 
-export type ExpressContext = inferAsyncReturnType<typeof createContext>;
-export type WebhookRequest = IncomingMessage & {rawBody: Buffer}
+export type ExpressContext = inferAsyncReturnType<typeof createContext>
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer }
 const start = async () => {
-
   const webhookMiddleware = bodyParser.json({
-    verify: (req: WebhookRequest, _, buffer) =>{
+    verify: (req: WebhookRequest, _, buffer) => {
       req.rawBody = buffer
-    }
+    },
   })
   app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler)
-
 
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
       onInit: async (cms: {
-        logger: { info: (arg0: string) => void };
-        getAdminURL: () => any;
+        logger: { info: (arg0: string) => void }
+        getAdminURL: () => any
       }) => {
-        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
       },
     },
-  });
+  })
 
-  if(process.env.NEXT_BUILD) {
-    app.listen(PORT, async() => {
+  const cartRouter = express.Router()
+  cartRouter.use(payload.authenticate)
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest
+    if (!request.user) return res.redirect("/sign-in?origin=cart")
+    const parsedUrl = parse(req.url, true)
+    return nextApp.render(req, res, "/cart", parsedUrl.query)
+  })
+  app.use("/cart", cartRouter)
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
       payload.logger.info("Next.js is building for production")
       // @ts-expect-error
-      await nextBuild(path.join(__dirname, '../'))
+      await nextBuild(path.join(__dirname, "../"))
       process.exit()
     })
-    return 
+    return
   }
-
 
   app.use(
     "/api/trpc",
@@ -62,18 +71,18 @@ const start = async () => {
       router: appRouter,
       createContext,
     })
-  );
-  app.use((req, res) => nextHandler(req, res));
+  )
+  app.use((req, res) => nextHandler(req, res))
 
   nextApp.prepare().then(() => {
-    payload.logger.info("Next.js started");
+    payload.logger.info("Next.js started")
 
     app.listen(PORT, async () => {
       payload.logger.info(
         `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-      );
-    });
-  });
-};
+      )
+    })
+  })
+}
 
-start();
+start()
